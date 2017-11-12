002fc72c58bffd50900ffc17ccf699196b8d04f3
Ivan Cui <ivancjr@uw.edu>
2017-11-11 22:22:35 -0800

    making change to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name="maze",
+    author="Ivan Cui"
     version='1.0',
+    url="https://github.com/whosivan/LM"
     py_modules=['lasermaze'],
     install_requires=[
         'Click',
